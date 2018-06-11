73afc752b8a27fda99236abe90c9f759389004d6
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-06-11 14:13:30 +0100

    get rid of global vars

--- examples/customers/main.go
+++ examples/customers/main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"gopkg.in/alecthomas/kingpin.v2"
 	"github.com/astec/tinyetl/examples/customers/customerscli"
-)
-
-var (
-	input   = kingpin.Flag("input", "Input file or URL").Short('i').String()
-	sorting = kingpin.Flag("sort", "Specifies how to sort customers: id, name. Prepend '-' for descending order.").Short('s').String()
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
-	c := context.Background()
-	_ = kingpin.Parse()
+	var (
+		input   = kingpin.Flag("input", "Input file or URL").Short('i').String()
+		sorting = kingpin.Flag("sort", "Specifies how to sort customers: id, name. Prepend '-' for descending order.").Short('s').String()
+	)
+	_ = kingpin.Parse() // Parses CLI input arguments
 
 	// Creates workflow defined by chain of workers.
 	// Initialization is in separate package to support unit-testing.
@@ -23,10 +21,10 @@ func main() {
 	// Create initial work items as list of files to process.
 	fileNamesIterator := customerscli.GetFileNamesIterator(*input)
 
+	c := context.Background()
 	err := workflow.Execute(c, fileNamesIterator) // Run processing
 
 	if err != nil { // Verify for error and report if needed
 		fmt.Println("ERROR:", err)
 	}
 }
-
